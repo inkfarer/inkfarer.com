8d3fcf4e8a6371d6d3bcb928b1d13816fea480bf
inkfarer <inkfarer@inkfarer.com>
2020-08-06 00:08:06 +0300

    Updates

---
+++ precache-manifest.a71f94eede79389b868b51cc40baa936.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0c5598df2dc0306a9c4b998a21f4e0",
+    "url": "/inkfarer.com/index.html"
+  },
+  {
+    "revision": "17e4b9366a018a87eec1",
+    "url": "/inkfarer.com/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6b5500669e3dfc8d80e3",
+    "url": "/inkfarer.com/static/css/main.71f4f84b.chunk.css"
+  },
+  {
+    "revision": "17e4b9366a018a87eec1",
+    "url": "/inkfarer.com/static/js/2.06433e11.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/inkfarer.com/static/js/2.06433e11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5500669e3dfc8d80e3",
+    "url": "/inkfarer.com/static/js/main.711b8acb.chunk.js"
+  },
+  {
+    "revision": "2ef949cd4a79b0cecd87",
+    "url": "/inkfarer.com/static/js/runtime-main.d2b3dbbb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2dc649163e268b322a3daae15172bb5f.js"
+  "/inkfarer.com/precache-manifest.a71f94eede79389b868b51cc40baa936.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/inkfarer.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
